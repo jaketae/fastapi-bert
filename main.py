eac09ca2fafa48752185240436646101b5cfcfcb
Jake Tae <jaesungtae@gmail.com>
2021-02-02 23:01:14 +0900

    feature: integrate fastapi app w/ inference backend

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from backend import predict
+
 app = FastAPI()
+
+
+class Input(BaseModel):
+    passage: str
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+@app.post("/generate")
+async def generate(input_: Input):
+    text = input_.passage
+    prediction = predict(text)
+    return {"prob": prediction}
