e36b966bea59fde2d0d78ed7b7a4aedaeeb1a5a1
Jake Tae <jaesungtae@gmail.com>
2021-02-02 22:59:16 +0900

    feature: quantize loaded model for faster performance

--- backend.py
+++ backend.py
@@ -5,13 +5,16 @@ tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModelForSequenceClassification.from_pretrained(
     "custom-bert-base-uncased", num_labels=2
 )
-model.eval()
+quantized_model = torch.quantization.quantize_dynamic(
+    model, {torch.nn.Linear}, dtype=torch.qint8
+)
+quantized_model.eval()
 
 
 @torch.no_grad()
 def predict(text):
     tokens = tokenizer(text, truncation=True, return_tensors="pt")
-    output = model(**tokens)
+    output = quantized_model(**tokens)
     logits = output["logits"].detach()
     prediction = logits.softmax(dim=1)[0][1].item()
     return prediction
